ed0fed47b6d35433366e96274032dda50fc793b5
Home Account <Personal@BELC02KC1WLFFT3.sea.corp.expecn.com>
2013-04-03 20:24:15 -0700

    passing an integer to the java code instead of temp var

--- stop_sniffer.sh
+++ stop_sniffer.sh
@@ -20,9 +20,11 @@ NEXT_STOP_COUNT=`wc -l stop_log_rout_1`
 if [[ $STOP_COUNT != $NEXT_STOP_COUNT ]]; then
     echo changed 
     LAST_LINE=`tail -1 stop_log_rout_1`
-    echo $LAST_LINE
+#    echo $LAST_LINE
     BUS_ID=`echo $LAST_LINE | cut -d ' ' -f2`
-    echo $BUS_ID
+    echo real bus id $BUS_ID
+    echo "!! USING REAL LIKE BUS_ID: 1234 !!"
+    BUS_ID=1234
     echo "running our java code with BUS_ID: $BUS_ID" 
 else
     echo same
